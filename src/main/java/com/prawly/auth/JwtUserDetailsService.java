224d768c2faa05e6f833200629af6b432e19a9bf
gouravmoy <gouravmoy.mohanty91@gmail.com>
2024-04-09 22:54:56 +0200

    fix: removed unused file and commented out lines

--- src/main/java/com/prawly/auth/JwtUserDetailsService.java
+++ src/main/java/com/prawly/auth/JwtUserDetailsService.java
@@ -1,13 +1,9 @@
 package com.prawly.auth;
 
-import java.util.ArrayList;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.prawly.exceptions.UserAlreadyExists;
@@ -22,25 +18,11 @@ public class JwtUserDetailsService implements UserDetailsService {
 	@Autowired
 	UserRepository userRepository;
 
-	// @Autowired
-	// private PasswordEncoder bcryptEncoder;
-
 	@Override
 	public UserDetails loadUserByUsername(String username) {
 		var user = userRepository.findByLogin(username);
 		return user;
 	}
-
-	// @Override
-	// public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-	// 	if ("javainuse".equals(username)) {
-	// 		// return new User("javainuse",
-	// 		// "$2a$10$slYQmyNdGzTn7ZLBXBChFOC9f6kFjAqPhccnP6DxlWXx2lPk1C3G6",
-	// 		// new ArrayList<>());
-	// 	} else {
-	// 		throw new UsernameNotFoundException("User not found with username: " + username);
-	// 	}
-	//}
 
 	public User save(SignUpDTO userDTO) {
 		if (userRepository.findByLogin(userDTO.getUsername()) != null) {
